75ace032dc63d86bbb8749b963f235040f6e0acb
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-12 11:01:51 +0100

    OK

--- pageAccueil.js
+++ pageAccueil.js
@@ -14,7 +14,7 @@ window.addEventListener('load', () => {
     TL
     .staggerFrom(titreSpans, 1, {top: -50, opacity:0, ease: "power2.out"}, 0.3)
     .from(recherche, 1, {left: -200, opacity:0, ease:"power2.out"}, 1.3)
-    .from(button, 1, { opacity:0, ease: "power2.out"},'-=1')
+    .from(button, 1, { opacity:0, ease: "power0"},'-=1')
     .from(l1, 1, { width: 0, ease: "power2.out"}, '-=2')
     .from(l2, 1, { width: 0, ease: "power2.out"}, '-=2')
     .staggerFrom(media, 0.7, {right:-200, ease: "power2.out"}, 0.3, '-=2');
@@ -42,11 +42,6 @@ window.addEventListener('load', () => {
     .typeString('ainsi que le <strong style="color: #9300ff ">PHP</strong> !')
     .pauseFor(500)
     .typeString(' J\'ai également travaillé sur le framework <strong style="color: #3FB27F ">VueJS</strong> !')
-
-
-    
-
-
 
     .start()
 
