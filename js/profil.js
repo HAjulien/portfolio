b5f02884a9ffbd88b4d8e6ce69af2406a5b69453
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-03-13 09:53:46 +0100

    frise parcours

--- js/profil.js
+++ js/profil.js
@@ -1,5 +1,13 @@
 const tableau = document.getElementById('tableau').getContext('2d');
 
+// pour scrolltriger et gsap effet sur section parcours
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+const controller = new ScrollMagic.Controller();
+
+
+// tableau chart dans competences
+
 let myChart = new Chart(tableau, {
 
     type:"radar",
@@ -39,7 +47,7 @@ let myChart = new Chart(tableau, {
             r: {
               pointLabels: {
                 font: {
-                  size: 16
+                  size: 15
                 }
               },
               
@@ -49,3 +57,22 @@ let myChart = new Chart(tableau, {
     }
     
 });
+
+// effet gsap et scrolltriger sur parcours
+
+allBoxes.forEach(box => {
+    for (i = 0; i< allRonds.length; i++){
+
+        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
+
+            let scene = new ScrollMagic.Scene({
+                triggerElement: allRonds[i],
+                reverse: false
+            })
+            .setTween(tween)
+            // .addIndicators()
+            .addTo(controller)
+        }
+    }
+})
